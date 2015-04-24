02cec2556723dee69ff0926b16b037009b667df4
lima9 <max9@openmailbox.org>
2015-04-24 14:47:39 +0100

    API arreglado!

--- dat-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415gm2/dat/api/model/ThemeCollection.java
+++
@@ -1,5 +0,0 @@
-package edu.upc.eetac.dsa.dsaqp1415gm2.dat.api.model;
-
-public class ThemeCollection {
-
-}
--- dat-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415gm2/dat/api/model/Thread.java
+++ dat-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415gm2/dat/api/model/Thread.java
@@ -2,7 +2,27 @@ package edu.upc.eetac.dsa.dsaqp1415gm2.dat.api.model;
 
 import java.util.List;
 
+import javax.ws.rs.core.Link;
+
+import org.glassfish.jersey.linking.Binding;
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLinks;
+import org.glassfish.jersey.linking.InjectLink.Style;
+
+import edu.upc.eetac.dsa.dsaqp1415gm2.dat.api.MediaType;
+import edu.upc.eetac.dsa.dsaqp1415gm2.dat.api.PostResource;
+
 public class Thread {
+	public class PostCollection {
+		@InjectLinks({
+			@InjectLink(resource = PostResource.class, style = Style.ABSOLUTE, rel = "create-sting", title = "Create sting", type = MediaType.DAT_API_POST),
+			@InjectLink(value = "/stings?before={before}", style = Style.ABSOLUTE, rel = "previous", title = "Previous stings", type = MediaType.DAT_API_POST_COLLECTION, bindings = { @Binding(name = "before", value = "${instance.oldestTimestamp}") }),
+			@InjectLink(value = "/stings?after={after}", style = Style.ABSOLUTE, rel = "current", title = "Newest stings", type = MediaType.DAT_API_POST_COLLECTION, bindings = { @Binding(name = "after", value = "${instance.newestTimestamp}") }) })
+	private List<Link> links;
+
+	public List<Link> getLinks() {
+		return links;
+	}
 	private String subject;
 	private String content;
 	private int idthread;
@@ -46,5 +66,5 @@ public class Thread {
 	public void setIdpost(int idpost) {
 		this.idpost = idpost;
 	}
-	
+	}
 }
