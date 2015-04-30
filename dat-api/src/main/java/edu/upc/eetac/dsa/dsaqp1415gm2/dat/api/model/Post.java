2d97d3f3d547f4dd4d52a8e679bb8dd54625de3d
Manel <maneeel93@gmail.com>
2015-04-30 12:03:32 +0200

    Java Resources

---
+++ dat-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415gm2/dat/api/model/Post.java
@@ -0,0 +1,20 @@
+package edu.upc.eetac.dsa.dsaqp1415gm2.dat.api.model;
+
+public class Post {
+	private String content;
+	private int idpost;
+	
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public int getIdpost() {
+		return idpost;
+	}
+	public void setIdpost(int idpost) {
+		this.idpost = idpost;
+	}
+	
+}
---
+++ dat-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415gm2/dat/api/model/User.java
@@ -0,0 +1,13 @@
+package edu.upc.eetac.dsa.dsaqp1415gm2.dat.api.model;
+
+public class User {
+	private String name = "Anonymous";
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
