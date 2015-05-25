caf99a419d590e3105a926c40c678492181bff7f
Manel <maneeel93@gmail.com>
2015-05-25 03:06:30 +0200

    hay fallos en datandroid

--- dat-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415gm2/dat/api/ThemeResource.java
+++ dat-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415gm2/dat/api/ThemeResource.java
@@ -26,7 +26,127 @@ public class ThemeResource {
 		Theme api = new Theme();
 		return api;
 	}
+	
 	@GET
+	@Path("/{nametheme}")
+	@Produces(MediaType.DAT_API_THREAD)
+	public Theme getthreads(@PathParam("nametheme") String nametheme) {
+		Theme theme = new Theme();
+		Connection conn = null;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServerErrorException("Could not connect to the database",
+					Response.Status.SERVICE_UNAVAILABLE);
+		}
+
+		PreparedStatement stmt = null;
+		try {
+			stmt = conn.prepareStatement(GET_THREADS_QUERY);
+			if (nametheme.equals("Tecnologia"))
+			{
+				stmt.setInt(1, Integer.valueOf(1));
+			}
+			if (nametheme.equals("Deportes"))
+			{
+				stmt.setInt(1, Integer.valueOf(2));
+			}
+			if (nametheme.equals("Motor"))
+			{
+				stmt.setInt(1, Integer.valueOf(3));
+			}
+			if (nametheme.equals("Videojuegos"))
+			{
+				stmt.setInt(1, Integer.valueOf(4));
+			}
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Threadx threadx= new Threadx();
+				threadx.setIdtema(rs.getInt("idtema"));
+				threadx.setIdthread(rs.getInt("idthread"));
+				threadx.setSubject(rs.getString("subject"));
+				threadx.setContent(rs.getString("content"));
+				String temp = rs.getString("imagen");
+				threadx.setImagen(temp);
+
+				theme.addThread(threadx);
+			}
+
+		} catch (SQLException e) {
+			throw new ServerErrorException(e.getMessage(),
+					Response.Status.INTERNAL_SERVER_ERROR);
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+		return theme;
+	}
+	//para obtener un thread poniendo la id del tema y del thread
+	@GET
+	@Path("/{nametheme}/{idhilo}")
+	@Produces("application/json")
+	public Threadx getpostwithIDT(@PathParam("nametheme") String nametheme, @PathParam("idhilo") String idhilo) {
+		Threadx thread = new Threadx();
+		int idthema = 0;
+		if (nametheme.equals("Tecnologia"))
+		{
+			idthema = 1;
+		}
+		if (nametheme.equals("Deportes"))
+		{
+			idthema = 2;
+		}
+		if (nametheme.equals("Motor"))
+		{
+			idthema = 3;
+		}
+		if (nametheme.equals("Videojuegos"))
+		{
+			idthema = 4;
+		}
+		Connection conn = null;
+		try {
+			conn = ds.getConnection();
+		} catch (SQLException e) {
+			throw new ServerErrorException("Could not connect to the database",
+					Response.Status.SERVICE_UNAVAILABLE);
+		}
+	 
+		PreparedStatement stmt = null;
+		try {
+			stmt = conn.prepareStatement(GET_POST_BY_IDS_QUERY);
+			stmt.setInt(1, idthema);
+			stmt.setInt(2, Integer.valueOf(idhilo));
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+				Post post = new Post();
+				post.setIdthema(rs.getInt("idthema"));
+				post.setIdhilo(rs.getInt("idhilo"));
+				post.setIdpost(rs.getInt("idpost"));
+				post.setContent(rs.getString("content"));
+				post.setImagelink(rs.getString("image_link"));
+				thread.addPost(post);
+			}
+		} 
+			catch (SQLException e) {
+			throw new ServerErrorException(e.getMessage(),
+					Response.Status.INTERNAL_SERVER_ERROR);
+		} finally {
+			try {
+				if (stmt != null)
+					stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+			}
+		}
+	 
+		return thread;
+	}
+	/*@GET
 	@Path("/Tecnologia")
 	@Produces(MediaType.DAT_API_THREAD)
 	public Theme getthreads() {
@@ -193,124 +313,5 @@ public class ThemeResource {
 			}
 		}
 		return theme;
-	}
-	/*@GET
-	@Path("/{nametheme}")
-	@Produces(MediaType.DAT_API_THREAD)
-	public Theme getthreads(@PathParam("nametheme") String nametheme) {
-		Theme theme = new Theme();
-		Connection conn = null;
-		try {
-			conn = ds.getConnection();
-		} catch (SQLException e) {
-			throw new ServerErrorException("Could not connect to the database",
-					Response.Status.SERVICE_UNAVAILABLE);
-		}
-
-		PreparedStatement stmt = null;
-		try {
-			stmt = conn.prepareStatement(GET_THREADS_QUERY);
-			if (nametheme.equals("Tecnologia"))
-			{
-				stmt.setInt(1, Integer.valueOf(1));
-			}
-			if (nametheme.equals("Deportes"))
-			{
-				stmt.setInt(1, Integer.valueOf(2));
-			}
-			if (nametheme.equals("Motor"))
-			{
-				stmt.setInt(1, Integer.valueOf(3));
-			}
-			if (nametheme.equals("Videojuegos"))
-			{
-				stmt.setInt(1, Integer.valueOf(4));
-			}
-			ResultSet rs = stmt.executeQuery();
-			while (rs.next()) {
-				Threadx threadx= new Threadx();
-				threadx.setIdtema(rs.getInt("idtema"));
-				threadx.setIdthread(rs.getInt("idthread"));
-				threadx.setSubject(rs.getString("subject"));
-				threadx.setContent(rs.getString("content"));
-				String temp = rs.getString("imagen");
-				threadx.setImagen(temp);
-
-				theme.addThread(threadx);
-			}
-
-		} catch (SQLException e) {
-			throw new ServerErrorException(e.getMessage(),
-					Response.Status.INTERNAL_SERVER_ERROR);
-		} finally {
-			try {
-				if (stmt != null)
-					stmt.close();
-				conn.close();
-			} catch (SQLException e) {
-			}
-		}
-		return theme;
 	}*/
-	//para obtener un thread poniendo la id del tema y del thread
-	@GET
-	@Path("/{nametheme}/{idhilo}")
-	@Produces("application/json")
-	public Threadx getpostwithIDT(@PathParam("nametheme") String nametheme, @PathParam("idhilo") String idhilo) {
-		Threadx thread = new Threadx();
-		int idthema = 0;
-		if (nametheme.equals("Tecnologia"))
-		{
-			idthema = 1;
-		}
-		if (nametheme.equals("Deportes"))
-		{
-			idthema = 2;
-		}
-		if (nametheme.equals("Motor"))
-		{
-			idthema = 3;
-		}
-		if (nametheme.equals("Videojuegos"))
-		{
-			idthema = 4;
-		}
-		Connection conn = null;
-		try {
-			conn = ds.getConnection();
-		} catch (SQLException e) {
-			throw new ServerErrorException("Could not connect to the database",
-					Response.Status.SERVICE_UNAVAILABLE);
-		}
-	 
-		PreparedStatement stmt = null;
-		try {
-			stmt = conn.prepareStatement(GET_POST_BY_IDS_QUERY);
-			stmt.setInt(1, idthema);
-			stmt.setInt(2, Integer.valueOf(idhilo));
-			ResultSet rs = stmt.executeQuery();
-			while (rs.next()) {
-				Post post = new Post();
-				post.setIdthema(rs.getInt("idthema"));
-				post.setIdhilo(rs.getInt("idhilo"));
-				post.setIdpost(rs.getInt("idpost"));
-				post.setContent(rs.getString("content"));
-				post.setImagelink(rs.getString("image_link"));
-				thread.addPost(post);
-			}
-		} 
-			catch (SQLException e) {
-			throw new ServerErrorException(e.getMessage(),
-					Response.Status.INTERNAL_SERVER_ERROR);
-		} finally {
-			try {
-				if (stmt != null)
-					stmt.close();
-				conn.close();
-			} catch (SQLException e) {
-			}
-		}
-	 
-		return thread;
-	}
 }
