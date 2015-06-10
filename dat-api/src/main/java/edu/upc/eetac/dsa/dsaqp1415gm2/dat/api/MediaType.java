cd4c37f1b7243a7fdfcfa55d0fa9019f6ff82e44
lima9 <max9@openmailbox.org>
2015-06-11 00:45:09 +0200

    UserResource afegit

--- dat-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415gm2/dat/api/MediaType.java
+++ dat-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415gm2/dat/api/MediaType.java
@@ -5,7 +5,8 @@ public interface MediaType {
   public final static String DAT_API_POST = "application/vnd.dat.api.post+json";
   public final static String DAT_API_THREAD = "application/vnd.dat.api.thread+json";
   public final static String DAT_API_THEME = "application/vnd.dat.api.theme+json";
-  public final static String DAT_API_ERROR = "application/vnd.dsa.beeter.error+json";
+  public final static String DAT_API_ERROR = "application/vnd.dsa.dat.error+json";
+
   
   
   
