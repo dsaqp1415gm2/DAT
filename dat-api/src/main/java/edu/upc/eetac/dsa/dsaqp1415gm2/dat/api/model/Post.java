07bd098d3e6b1e24f197dd87b469834d3e35e2cd
Manel <maneeel93@gmail.com>
2015-06-13 17:54:18 +0200

    links en post

--- dat-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415gm2/dat/api/model/Post.java
+++ dat-api/src/main/java/edu/upc/eetac/dsa/dsaqp1415gm2/dat/api/model/Post.java
@@ -1,11 +1,25 @@
 package edu.upc.eetac.dsa.dsaqp1415gm2.dat.api.model;
 
+import java.util.List;
+
+import javax.ws.rs.core.Link;
+
+import org.glassfish.jersey.linking.Binding;
+import org.glassfish.jersey.linking.InjectLink;
+import org.glassfish.jersey.linking.InjectLinks;
+import org.glassfish.jersey.linking.InjectLink.Style;
+
+import edu.upc.eetac.dsa.dsaqp1415gm2.dat.api.MediaType;
+
 public class Post {
+	@InjectLinks({
+		@InjectLink(value = "/thread/post/{idpost}", style = Style.ABSOLUTE, rel = "idpost", title = "post link", type = MediaType.DAT_API_THREAD,bindings = { @Binding(name = "idpost", value = "${instance.idpost}")})})
 	private String content;
 	private int idpost;
 	private int idthema;
 	private int idhilo;
 	private String imagelink;
+	private List<Link> links;
 	
 	public String getContent() {
 		return content;
@@ -36,6 +50,12 @@ public class Post {
 	}
 	public void setImagelink(String imagelink) {
 		this.imagelink = imagelink;
+	}
+	public List<Link> getLinks() {
+		return links;
+	}
+	public void setLinks(List<Link> links) {
+		this.links = links;
 	}
 	
 }
